5d449cda8479f2fa8c7cce3cec7a06b78e2ff06e
Gonza97 <chateas.con.gonzy@hotmail.com>
2017-09-28 12:20:36 -0300

    Add files via upload

---
+++ Relaciones muchos a muchos/main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+typedef struct
+{
+    int idLaboratorio;
+    int RazonSocial;
+}eLaboratorio;
+
+typedef struct
+{
+    int idMedicamento;
+    char descripcion[][]
+}
+
+typedef struct
+
+int main()
+{
+    printf("Hello world!\n");
+    return 0;
+}
